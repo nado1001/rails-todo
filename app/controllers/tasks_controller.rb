27318768c3a4fba485979bcecab16ff21aecaa3e
take-tech1001 <tk.tech1001@gmail.com>
2021-07-07 00:53:45 +0900

    Added create action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,4 +7,14 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
+
+  def create
+    @task = Task.create(task_params)
+    redirect_to task_path
+  end
+
+  private
+    def task_params
+      params.require(:task).permit(:title)
+    end
 end
